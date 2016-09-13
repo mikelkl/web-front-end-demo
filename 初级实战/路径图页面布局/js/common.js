bd1b7a0482013cf045a6e53baeb76f3eed41bcf8
余罪 <kanglong.lkl@alibaba-inc.com>
2016-09-13 16:49:07 +0800

    课程盒子加入动画效果

---
+++ 初级实战/路径图页面布局/js/common.js
@@ -0,0 +1,34 @@
+/**
+ * Created by kanglong.lkl on 2016/9/13.
+ */
+var common = {
+    init: function () {
+        this.lessImgShow();
+    },
+    stopEventBubble: function (event) {
+        var e = event;
+        if (e) {
+            e.stopPropagation();
+        }
+    },
+    lessImgShow: function () {
+        $(".lessonimgbox").bind("mouseover", function (e) {
+            var _pop = $(this).find(".img-pop");
+            $(this).css({border: "2px solid #1abc9c", width: "226px", height: "226px"});
+            TweenMax.to(_pop, 0.5, {top: 0, ease: Bounce.easeOut});
+            common.stopEventBubble(e);
+        });
+
+        $(document).bind("mouseover", function () {
+            $(".lessonimgbox").css({border: "0", width: "230px", height: "230px"});
+            var _pop = $(this).find(".img-pop");
+            TweenMax.to(_pop, 0.5, {top: 230, ease: Linear.easeNone});
+        })
+
+    },
+
+}
+
+$(function () {
+    common.init();
+})
